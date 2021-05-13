dc1af9c1ae11bf5bf3cdcd8184497d75619bccc0
SylviaHorvath <81666834+SylviaHorvath@users.noreply.github.com>
2021-05-12 21:41:32 -0700

    Add files via upload

---
+++ template.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ace In The Hole Multisport Events</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <nav id="ham_nav">
+        <!-- [THE HAMBURGER] -->
+        <label for="hamburger">&#9776;</label>
+        <input type="checkbox" id="hamburger" />
+
+        <!-- [MENU ITEMS] -->
+        <div id="ham_items">
+            <a href="#">HOME</a>
+            <a href="#">F.A.Q.</a>
+            <a href="#">GALLERY</a>
+            <a href="#">REGISTER</a>
+            <a href="#">CONTACT</a>
+        </div>
+    </nav>
+
+</head>
+
+<body>
+<main>
+<div class="header_container">
+    <h1>Lorem Ipsum</h1>
+</div>
+<div class="body_container">
+    <p>Lorem Ipsum.</p>
+</div>
+<hr>
+<?php
+
+?>
+</main>
+</body>
+<footer>
+    <p> 2021 | Site by Sylvia Horvath</p>
+</footer>
+<br>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</html>
